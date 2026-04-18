73c7bf382c590f9a2a4aa5cbbde49e9a065efbe0
Yenleak <y_sailaukhan@kbtu.kz>
2026-04-18 15:58:27 +0600

    CustomUser to admin panel

--- back/core/admin.py
+++ back/core/admin.py
@@ -1,10 +1,14 @@
+# # Register your models here.
 from django.contrib import admin
-from .models import Workspace, WorkspaceMember, Category, Task, SubTask, Comment, TaskActivity
+from .models import CustomUser  # Импортируем модель пользователя
 
-admin.site.register(Workspace)
-admin.site.register(WorkspaceMember)
-admin.site.register(Category)
-admin.site.register(Task)
-admin.site.register(SubTask)
-admin.site.register(Comment)
-admin.site.register(TaskActivity)
+# Вариант 1: Простая регистрация (выведет просто список)
+# admin.site.register(CustomUser)
+
+# Вариант 2: Красивая регистрация 
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    # Указываем, какие колонки показывать в таблице админки
+    list_display = ('id', 'email', 'name', 'is_staff', 'is_active') 
+    # Добавляем поиск по email и имени
+    search_fields = ('email', 'name')
