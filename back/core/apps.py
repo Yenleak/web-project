334e07706e154dead501f96a79d6434ae6a89d95
Sanzhar <sanzhar.saparkhan@mail.ru>
2026-04-22 05:40:30 +0500

    fixed spelling error

--- back/core/apps.py
+++ back/core/apps.py
@@ -16,5 +16,5 @@ class CoreConfig(AppConfig):
         if os.environ.get("RUN_MAIN") != "true":
             return
 
-        from .scheduler import start
+        from .schedular import start
         start()
